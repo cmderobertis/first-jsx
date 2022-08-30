e92966a298d582d6c0ab4e98599c6d79621a0e41
cmderobertis <cmderobertis@gmail.com>
2022-08-30 10:31:45 -0700

    updated landing page

---
+++ src/components/App.js
@@ -0,0 +1,21 @@
+import "../App.css"
+import React from "react"
+
+class App extends React.Component {
+  render() {
+    return (
+      <>
+        <h1 class="title">Hello Dojo!</h1>
+        <h2>Things I need to do:</h2>
+        <ul>
+          <li>Learn React</li>
+          <li>Build some apps</li>
+          <li>Contribute to an Open Source Project</li>
+          <li>Apply for jobs</li>
+        </ul>
+      </>
+    )
+  }
+}
+
+export default App
